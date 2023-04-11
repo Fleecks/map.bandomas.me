48574a9c01c8569c40b47e0fdd94d8afb27c85ca
Fleecks <78699409+Fleecks@users.noreply.github.com>
2023-04-11 16:04:34 +0300

    Create unmined.map.properties.js

---
+++ js/unmined.map.properties.js
@@ -0,0 +1,14 @@
+var UnminedMapProperties = {
+	minZoom: -5,
+	maxZoom: 7,
+	defaultZoom: 0,
+	imageFormat: "webp",
+	minRegionX: -22,
+	minRegionZ: -21,
+	maxRegionX: 20,
+	maxRegionZ: 20,
+	worldName: "Bandomas SEASON 6",
+	background: "#ccc",
+	spawnersMarkers: new Array(),
+	strongholdsMarkers: new Array()
+}
